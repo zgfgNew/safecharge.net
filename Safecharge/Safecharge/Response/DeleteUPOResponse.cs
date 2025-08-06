c027945a66bd2f6c99522db25c6303330358977d
zgfgNew <ivo.penzar+PaySafeCard-test@soar.hr>
2025-08-06 15:01:40 +0200

    Add files via upload

---
+++ Safecharge/Safecharge/Response/DeleteUPOResponse.cs
@@ -0,0 +1,14 @@
+﻿using Safecharge.Response.Common;
+
+namespace Safecharge.Response
+{
+    // Extending Safecharge properties per https://docs.nuvei.com/api/advanced/indexAdvanced.html?json#deleteUPO
+    /// <summary>
+    /// Response received from the SafeCharge's servers to the <see cref="Request.DeleteUPORequest"/>.
+    /// </summary>
+    public class DeleteUPOResponse : SafechargeResponse
+    {
+        /// <summary>
+        /// </summary>
+    }
+}
